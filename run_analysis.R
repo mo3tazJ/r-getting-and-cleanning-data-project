b681356d4b9b0b1390669fe6745de651e6c60ecc
mo3tazJ <en.mo3taz@gmail.com>
2025-06-08 01:29:41 +0300

    Install and load required Packages

--- run_analysis.R
+++ run_analysis.R
@@ -1,2 +1,17 @@
 # Getting and Cleaning Data Course Project
 
+# creating Data directory if not existed
+if(!file.exists("./data")){dir.create("./data")}
+
+# Install Required Packages
+# install.packages("data.table")
+# install.packages("reshape2")
+
+# Load Packages
+library(data.table)  
+library(reshape2)
+
+# Load Packages (another method)
+# packages <- c("data.table", "reshape2")
+# sapply(packages, require, character.only=TRUE, quietly=TRUE)
+
